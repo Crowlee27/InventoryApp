b9e0f43139940a2ceaa47cdf4e954544ee546d24
Cory Rowlee <rowlee.development@gmail.com>
2023-08-03 01:40:11 -0500

    add textfield for dialog module

---
+++ Frontend-client/my-app/src/components/dialogForms/drawingDialog/drawingTextfield.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function DrawingNumberText() {
+  return <TextField label="Drawing #" fullWidth variant="standard" />;
+}
