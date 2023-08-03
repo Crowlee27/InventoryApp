3a02808f9bc9bf39a404d6c183ffa8e4a2ffb2ae
Cory Rowlee <rowlee.development@gmail.com>
2023-08-03 15:51:43 -0500

    testing components searchbars

--- Frontend-client/my-app/src/App.tsx
+++ Frontend-client/my-app/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { AddDrawingsForm } from "./components/dialogForms/drawingDialog";
+import { GridSearchBar } from "./components/navBars/gridBar/gridSearchBar";
+import { SearchBar } from "./components/navBars/searchBar/searchBar";
 
 function App() {
   return (
     <div>
-      <AddDrawingsForm />
+      {/* <AddDrawingsForm /> */}
+      <SearchBar />
+      <GridSearchBar/>
     </div>
   );
 }
