f4c1daf6499ca0f4e2492a65a5fdc028b7a36e7a
Cory Rowlee <rowlee.development@gmail.com>
2023-08-03 01:41:06 -0500

    added modules for dialog box

---
+++ Frontend-client/my-app/src/components/dialogForms/drawingDialog/drawingDialogForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DrawingTextfield from "./drawingTextfield";
+import SelectDrawingDescription from "./selectDrawingDesc";
+
+export default function DrawingsFormFields() { 
+    return (
+        <div>
+            <DrawingTextfield />
+            <SelectDrawingDescription />
+
+        </div>
+    )
+}
