3ab4bdc1079c7008dd3ce9ed4ef6ad9cae3bf1cc
Cory Rowlee <rowlee.development@gmail.com>
2023-08-03 15:53:20 -0500

    nested searchbar into grid header

---
+++ Frontend-client/my-app/src/components/navBars/gridBar/gridSearchBar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { SearchBar } from "../searchBar/searchBar";
+
+export const GridSearchBar = () => {
+  return (
+    <div className="gridSearchBar">
+      <div className="searchBar">
+        <SearchBar />
+        <Button variant="contained" color="primary" className="resetButton">
+          Reset
+        </Button>
+      </div>
+
+      <div>
+        <Button variant="contained" color="primary" className="CreateDrawing">
+          Create Drawing
+        </Button>
+      </div>
+    </div>
+  );
+};
