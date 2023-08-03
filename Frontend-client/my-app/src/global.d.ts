fc3bc7f4537a2d33515ce4a835ddb5a154f69242
Cory Rowlee <rowlee.development@gmail.com>
2023-08-03 17:41:55 -0500

    added interfaces

--- Frontend-client/my-app/src/global.d.ts
+++ Frontend-client/my-app/src/global.d.ts
@@ -1,12 +1,22 @@
 export {};
 
+declare global {
+  interface ISearchBar {
+    name: string;
+    label: string;
+    id: string;
+  }
 
-declare global{
+  interface IDrawingNumberText {
+    label: string;
+    id: string;
+    name: string;
+  }
 
-interface ISearchBar {
-  name: string;
-  label: string;
-  id: string;
+  interface IDrawingDescSelect {
+    label: string;
+    name: string;
+    id: string;
+    value: string;
+  }
 }
-
-};
