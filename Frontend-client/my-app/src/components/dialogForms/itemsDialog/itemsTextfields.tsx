e033501ddc0d270db93389e1bac7161da6cf1551
Cory Rowlee <rowlee.development@gmail.com>
2023-08-03 23:55:43 -0500

    re worked text fields

--- Frontend-client/my-app/src/components/dialogForms/itemsDialog/itemsTextfields.tsx
+++ Frontend-client/my-app/src/components/dialogForms/itemsDialog/itemsTextfields.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 
-export default function ItemsTextFields() {
+export const ItemsTextFields = ({ textFields }: ITextFieldsProps) => {
   return (
     <div>
-      <TextField label="Item Description" variant="standard" fullWidth />
-
-      <Stack direction={"row"} spacing={2}>
-        <TextField label="Size" variant="standard" />
-        <TextField label="Length" variant="standard" />
-        <TextField label="Rating" variant="standard" />
+      <Stack direction="row" spacing={2}>
+        {textFields.map((textField) => (
+          <TextField
+            key={textField.id}
+            label={textField.label}
+            variant="standard"
+            fullWidth
+            name={textField.name}
+            id={textField.id}
+          />
+        ))}
       </Stack>
-
-      <Stack direction={"row"} spacing={2}>
-        <TextField label="Serial" variant="standard" />
-        <TextField label="Tag #" variant="standard" />
-        <TextField label="Alias" variant="standard" />
-      </Stack>
-
-      <TextField label="Purchased" variant="standard" />
     </div>
   );
-}
+};
