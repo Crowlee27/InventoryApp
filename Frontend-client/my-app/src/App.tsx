f1a613e1922b2934919fe9d5c9cf2a6c77f0ee1c
Cory Rowlee <rowlee.development@gmail.com>
2023-08-04 15:43:09 -0500

    testing look of page

--- Frontend-client/my-app/src/App.tsx
+++ Frontend-client/my-app/src/App.tsx
@@ -3,6 +3,7 @@ import { BomGrid } from "./components/agGridTables/agGridBom/bomGrid";
 import { CatalogGrid } from "./components/agGridTables/agGridCatalog/catalogGrid";
 import { DrawingGrid } from "./components/agGridTables/agGridDrawing/drawingGrid";
 import { InventoryGrid } from "./components/agGridTables/agGridInventory/inventoryGrid";
+import { SearchAppBar } from "./components/navBars/appBar/appBar";
 import { AddDrawingsForm } from "./components/dialogForms/drawingDialog";
 import { GridSearchBar } from "./components/navBars/gridBar/gridSearchBar";
 import { SearchBar } from "./components/navBars/searchBar/searchBar";
@@ -10,13 +11,17 @@ import { SearchBar } from "./components/navBars/searchBar/searchBar";
 function App() {
   return (
     <div>
-      {/* <AddDrawingsForm />
-      <SearchBar />
-      <GridSearchBar /> */}
-      {/* <DrawingGrid/> */}
+      <SearchAppBar />
+      <h1>
+        Page Name
+      </h1>
+      {/* <AddDrawingsForm /> */}
+      {/* <SearchBar /> */}
+      <GridSearchBar />
+      <DrawingGrid/>
       {/* <CatalogGrid/> */}
       {/* <BomGrid/> */}
-      <InventoryGrid/>
+      {/* <InventoryGrid/> */}
     </div>
   );
 }
