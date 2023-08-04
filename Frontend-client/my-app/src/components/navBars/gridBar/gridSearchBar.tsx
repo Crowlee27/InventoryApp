c5b7607e7e41a8b644d3195f7742b23d2045d732
Cory Rowlee <rowlee.development@gmail.com>
2023-08-04 00:25:40 -0500

    update from searchbar

--- Frontend-client/my-app/src/components/navBars/gridBar/gridSearchBar.tsx
+++ Frontend-client/my-app/src/components/navBars/gridBar/gridSearchBar.tsx
@@ -7,17 +7,14 @@ export const GridSearchBar = () => {
   return (
     <div className="gridSearchBar">
       <div className="searchBar">
-        <SearchBar name="search" label="Search" id="search" />
+        <SearchBar />
         <Button variant="contained" color="primary" className="resetButton">
           Reset
         </Button>
       </div>
 
       <div>
-        {/* <Button variant="contained" color="primary" className="CreateDrawing">
-          Create Drawing
-        </Button> */}
-              <AddDrawingsForm/>
+        <AddDrawingsForm />
       </div>
     </div>
   );
