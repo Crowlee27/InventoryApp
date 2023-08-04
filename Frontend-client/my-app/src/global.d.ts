d66386f18f303a1de1c6d07190c7d0442b271f22
Cory Rowlee <rowlee.development@gmail.com>
2023-08-04 15:51:45 -0500

    added interfaces for AgGrid

--- Frontend-client/my-app/src/global.d.ts
+++ Frontend-client/my-app/src/global.d.ts
@@ -16,5 +16,38 @@ declare global {
     name: string;
     id: string;
     value: string;
-  }
+    }
+    
+    interface IDrawingGridRow {
+      ID: number;
+      Number: string;
+      Description: string;
+    }
+
+    interface ICatalogGridRow {
+      ID: number;
+      Description: string;
+      Size: string;
+      Length: string;
+      Rating: string;
+      Serial: string;
+    }
+
+    interface IBomGridRow {
+      ID: number;
+      Drawing: number;
+      Catalog: number;
+      Tag: string;
+      Alias: string;
+    }
+
+    interface IInventoryGridRow {
+      ID: number;
+      Bom: number;
+      Purchased: number;
+      Received: number;
+      Outstanding?: number;
+      Issued: number;
+      Remaining: number;
+    }
 }
