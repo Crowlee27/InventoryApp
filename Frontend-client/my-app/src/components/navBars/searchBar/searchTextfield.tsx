7402c13b3f6d7d3ac13d44e032954c94d010339a
Cory Rowlee <rowlee.development@gmail.com>
2023-08-04 00:22:26 -0500

    add search text field

---
+++ Frontend-client/my-app/src/components/navBars/searchBar/searchTextfield.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { TextField, Button } from "@mui/material";
+
+export const SearchTextfield = (props: ITextField) => {
+  return (
+    <div className="searchBar">
+      <TextField
+        id="search"
+        label={props.label}
+        name="search"
+        variant="outlined"
+        size="small"
+      />
+      <Button variant="contained" color="primary" className="searchButton">
+        Search
+      </Button>
+    </div>
+  );
+};
