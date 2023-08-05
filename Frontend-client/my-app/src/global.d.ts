11dde08cec1434acaa1001905a8ee1ba0083df8e
Cory Rowlee <rowlee.development@gmail.com>
2023-08-04 23:44:03 -0500

    added/fixed interfaces

--- Frontend-client/my-app/src/global.d.ts
+++ Frontend-client/my-app/src/global.d.ts
@@ -16,38 +16,48 @@ declare global {
     name: string;
     id: string;
     value: string;
-    }
-    
-    interface IDrawingGridRow {
-      ID: number;
-      Number: string;
-      Description: string;
-    }
+  }
 
-    interface ICatalogGridRow {
-      ID: number;
-      Description: string;
-      Size: string;
-      Length: string;
-      Rating: string;
-      Serial: string;
-    }
+  interface IDrawingGridRow {
+    ID: number;
+    Number: string;
+    Description: string;
+  }
 
-    interface IBomGridRow {
-      ID: number;
-      Drawing: number;
-      Catalog: number;
-      Tag: string;
-      Alias: string;
-    }
+  interface ICatalogGridRow {
+    ID: number;
+    Description: string;
+    Size: string;
+    Length: string;
+    Rating: string;
+    Serial: string;
+  }
 
-    interface IInventoryGridRow {
-      ID: number;
-      Bom: number;
-      Purchased: number;
-      Received: number;
-      Outstanding?: number;
-      Issued: number;
-      Remaining: number;
-    }
+  interface IBomGridRow {
+    ID: number;
+    Drawing: number;
+    Catalog: number;
+    Tag: string;
+    Alias: string;
+  }
+
+  interface IInventoryGridRow {
+    ID: number;
+    Bom: number;
+    Purchased: number;
+    Received: number;
+    Outstanding?: number;
+    Issued: number;
+    Remaining: number;
+  }
+
+  interface IDeleteButton {
+    onClick: () => void;
+  }
+
+  interface IDeleteConfirmationDialog {
+    open: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
+  }
 }
