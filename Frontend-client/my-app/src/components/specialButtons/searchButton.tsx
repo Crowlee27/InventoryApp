06ecc8916bff79c23c444423aba8b69551fb250e
Cory Rowlee <rowlee.development@gmail.com>
2023-08-10 01:18:22 -0500

    refactor

--- Frontend-client/my-app/src/components/specialButtons/searchButton.tsx
+++ Frontend-client/my-app/src/components/specialButtons/searchButton.tsx
@@ -1,7 +1,7 @@
-import React, { ReactElement } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 
-export const SearchButton = (props: ISearchButton): ReactElement => {
+export const SearchButton = (props: ISearchButton) => {
   const { onClick } = props;
 
   const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
